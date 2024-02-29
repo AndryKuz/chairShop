e630539b65b92d3216851671820804dcab1b31b5
AndryKuz <fmeon1992@gmail.com>
2024-02-29 18:36:17 +0200

    complete dinamic buttons

--- src/components/modal/ModalCenter.jsx
+++ src/components/modal/ModalCenter.jsx
@@ -1,33 +1,54 @@
-import { useDispatch, useSelector } from 'react-redux';
-import cl from './ModalCenter.module.scss'
-import { clearArrayFavorite } from './modalSlice';
+import { useEffect, useState } from "react";
+import { useDispatch } from "react-redux";
+import cl from "./ModalCenter.module.scss";
+import { clearArrayFavorite } from "./modalSlice";
 
-
-const ModalCenter = ({onHide}) => {
+const ModalCenter = ({ onHide, actionType }) => {
   const dispatch = useDispatch();
+  const [modalMessage, setModalMessage] = useState("");
+
+console.log(typeof(addAllToCart));
 
   const handleModal = () => {
     onHide();
-  }
-
+  };
 
   const clearItemWishlist = () => {
     dispatch(clearArrayFavorite());
     handleModal();
-  }
-  
+  };
+
+  const handleAction = () => {
+    if (actionType === "clearWishlist") {
+      clearItemWishlist();
+      setModalMessage("Wishlist has been cleared.");
+    } else if (actionType === "addAllToCart") {
+      // dispatch(addAllItemsToCart());
+      setModalMessage("All items have been added to cart.");
+    }
+    handleModal();
+
+    
+  };
+  useEffect(() => {
+    if (actionType === "clearWishlist") {
+      setModalMessage("Are you sure to empty your wishlist?");
+    } else if (actionType === "addAllToCart") {
+      setModalMessage("Add all items to cart?");
+    }
+  }, [actionType]);
+
   return (
     <>
-    <div className="overlay" onClick={handleModal}/>
-    <div className={cl.wrapperModalCenter}>
-      <h5>Are you sure to empty your wishlist?</h5>
-      <div className={cl.modalButton}>
-        <button onClick={handleModal}>Cancel</button>
-        <button onClick={clearItemWishlist}>Ok</button>
+      <div className="overlay" onClick={handleModal} />
+      <div className={cl.wrapperModalCenter}>
+        <h5>{modalMessage}</h5>
+        <div className={cl.modalButton}>
+          <button onClick={handleModal}>Cancel</button>
+          <button onClick={handleAction}>Ok</button>
+        </div>
       </div>
-    </div>
     </>
-    
   );
 };
 
--- src/pages/Wishlist.jsx
+++ src/pages/Wishlist.jsx
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import cl from "../../src/assets/styles/Wishlist.module.scss";
 import ButtonDinamic from "../components/buttons/ButtonDinamic";
@@ -7,13 +8,12 @@ import {
 } from "../components/modal/modalSlice";
 import { AiFillDelete } from "react-icons/ai";
 import { BsCart4 } from "react-icons/bs";
-import { Link } from "react-router-dom";
-import { useState } from "react";
 import ModalCenter from "../components/modal/ModalCenter";
 
 const Wishlist = () => {
   const dispatch = useDispatch();
   const [isVisible, setIsVisible] = useState(false);
+  const [actionType, setActionType] = useState("");
   const { favorite } = useSelector((state) => state.modal);
 
   const removeItem = (id) => {
@@ -24,93 +24,105 @@ const Wishlist = () => {
     dispatch(addItemToFavorite({ ...item, quantity }));
   };
 
-  const showModal = () => {
+  const showModal = (type) => {
+    setActionType(type);
     setIsVisible(true);
-  }
+  };
   const hideModal = () => {
     setIsVisible(false);
-  }
+  };
 
   return (
     <section>
       <div className="container">
         <div className={cl.mainContent}>
           <div className={cl.title}>Wishlist</div>
-          <div className={cl.productWishlist}>
-            <div className={cl.descriptionItem}>
-              <div className={cl.empty}></div>
-              <div className={cl.prodDes}>Product</div>
-              <div className={cl.priceDes}>Price</div>
-              <div className={cl.qtyDes}>Qty</div>
-              <div className={cl.subtotalDes}>Subtotal</div>
-              <div className={cl.empty}></div>
-            </div>
-            <div className={cl.cardWishlist}>
-              {favorite.map((prod) => {
-                const { title, images, price, id, quantity } = prod;
+          {favorite.length === 0 ? (
+            <div>You have no items in your wishlist.</div>
+          ) : (
+            <div className={cl.productWishlist}>
+              <div className={cl.descriptionItem}>
+                <div className={cl.empty}></div>
+                <div className={cl.prodDes}>Product</div>
+                <div className={cl.priceDes}>Price</div>
+                <div className={cl.qtyDes}>Qty</div>
+                <div className={cl.subtotalDes}>Subtotal</div>
+                <div className={cl.empty}></div>
+              </div>
+              <div className={cl.cardWishlist}>
+                {favorite.map((prod) => {
+                  const { title, images, price, id, quantity } = prod;
 
-                return (
-                  <div className={cl.itemWishlist} key={id}>
-                    <div className={cl.imageProd}>
-                      <img src={images[0]} alt={title} />
+                  return (
+                    <div className={cl.itemWishlist} key={id}>
+                      <div className={cl.imageProd}>
+                        <img src={images[0]} alt={title} />
+                      </div>
+                      <div className={cl.titleProd}>{title}</div>
+                      <div className={cl.priceProd}>{price}</div>
+                      <div className={cl.quantityProd}>
+                        <div
+                          className={cl.minus}
+                          onClick={() =>
+                            changeQuantity(prod, Math.max(1, quantity - 1))
+                          }
+                        >
+                          <ButtonDinamic
+                            isPlus={false}
+                            widthCircle="20px"
+                            hightCircle="20px"
+                            svgSize="20px"
+                          />
+                        </div>
+                        <span className={cl.quantityWishlist}>{quantity}</span>
+                        <div
+                          className={cl.plus}
+                          onClick={() =>
+                            changeQuantity(prod, Math.max(1, quantity + 1))
+                          }
+                        >
+                          <ButtonDinamic
+                            isPlus={true}
+                            widthCircle="20px"
+                            hightCircle="20px"
+                            svgSize="20px"
+                          />
+                        </div>
+                      </div>
+                      <div className={cl.subtotalProd}>
+                        $ {price * quantity}
+                      </div>
+                      <div className={cl.addRemove}>
+                        <div className={cl.cartAddWithFav}>
+                          <BsCart4 />
+                          <div className={cl.tooltip}>Add to Cart</div>
+                        </div>
+                        <div
+                          className={cl.removeProd}
+                          onClick={() => removeItem(id)}
+                        >
+                          <AiFillDelete />
+                          <div className={cl.tooltip}>Delete</div>
+                        </div>
+                      </div>
                     </div>
-                    <div className={cl.titleProd}>{title}</div>
-                    <div className={cl.priceProd}>{price}</div>
-                    <div className={cl.quantityProd}>
-                      <div
-                        className={cl.minus}
-                        onClick={() =>
-                          changeQuantity(prod, Math.max(1, quantity - 1))
-                        }
-                      >
-                        <ButtonDinamic
-                          isPlus={false}
-                          widthCircle="20px"
-                          hightCircle="20px"
-                          svgSize="20px"
-                        />
-                      </div>
-                      <span className={cl.quantityWishlist}>{quantity}</span>
-                      <div
-                        className={cl.plus}
-                        onClick={() =>
-                          changeQuantity(prod, Math.max(1, quantity + 1))
-                        }
-                      >
-                        <ButtonDinamic
-                          isPlus={true}
-                          widthCircle="20px"
-                          hightCircle="20px"
-                          svgSize="20px"
-                        />
-                      </div>
-                    </div>
-                    <div className={cl.subtotalProd}>$ {price * quantity}</div>
-                    <div className={cl.addRemove}>
-                      <div className={cl.cartAddWithFav}>
-                        <BsCart4 />
-                        <div className={cl.tooltip}>Add to Cart</div>
-                      </div>
-                      <div
-                        className={cl.removeProd}
-                        onClick={() => removeItem(id)}
-                      >
-                        <AiFillDelete />
-                        <div className={cl.tooltip}>Delete</div>
-                      </div>
-                    </div>
-                  </div>
-                );
-              })}
+                  );
+                })}
+              </div>
+              <div className={cl.final}>
+                <button onClick={() => showModal("clearWishlist")}>
+                  {" "}
+                  empty wishlist
+                </button>
+                <button onClick={() => showModal("addAllToCart")}>
+                  add all to cart
+                </button>
+                {isVisible && (
+                  <ModalCenter onHide={hideModal} actionType={actionType} />
+                )}
+              </div>
             </div>
-            <div className={cl.final}>
-                <button onClick={showModal}> empty wishlist</button>
-                {isVisible && <ModalCenter onHide={hideModal}/>}
-              <Link>
-                <button>add all to cart</button>
-              </Link>
-            </div>
-          </div>
+          )}
         </div>
       </div>
     </section>
